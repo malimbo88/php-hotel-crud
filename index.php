3b43dfc529530011d8c43050a1b358d1fcb43d17
Nicola <nicolavalente88@gmail.com>
2020-07-27 15:56:19 +0200

    Prova 2

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>php-hotel-crud</title>
+
+    <!-- Link Html -->
+    <link rel="stylesheet" href="dist/style.css">
+    <!-- Link Html -->
+
+  </head>
+  <body>
+
+    <!-- header -->
+    <header>
+      <h1>Boolean Hotel</h1>
+    </header>
+    <!-- end header -->
+    
+    <!-- main -->
+    <main>
+    </main>
+    <!-- end main -->
+
+
+  </body>
+</html>
